0c82b8d4ad207ec0d814a08f8a13aa43fd52e4b8
Fraser Elliott <fraserelliott@hotmail.com>
2025-08-12 21:20:32 +0100

    updated Header to include a logo, tagline and navigation. Changed global variables to placeholders that work with the logo.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,53 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
+import styles from "./Header.module.css";
+import { useAuth } from '../contexts/AuthContext';
 
 export const Header = () => {
   return (
-    <header>
-      <Link to="/">Home</Link>
+    <header className={styles.container}>
+      <div className={styles.logoGroup}>
+        <Link to="/">
+          <img src={logo} height="50" />
+        </Link>
+        <h1>Build your pet's online portfolio.</h1>
+      </div>
+      <Navbar />
+      <div>
+        Placeholder
+      </div>
     </header>
   );
+};
+
+const pages = [
+  {name: "Home", to: "/"},
+  {name: "Following", to: "/following"},
+];
+
+const Navbar = () => {
+  const { token } = useAuth();
+
+  const renderPageLink = (name, to) => {
+    return (
+      <li
+        key={name}>
+        <NavLink
+          to={to}
+          className={({isActive}) =>
+            isActive ? `${styles.navItem} ${styles.selected}` : styles.navItem
+          }
+        >
+          {name}
+        </NavLink>
+      </li>
+    );
+  };
+
+  return (
+    <ul className={styles.navList}>
+      { renderPageLink('Home', '/')}
+      { token && renderPageLink('Following', '/following')}
+    </ul>
+  )
 };
